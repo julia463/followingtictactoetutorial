7372606f5b5693bd41c402f5d9c9de4b30196773
julia463 <54084784+julia463@users.noreply.github.com>
2023-05-10 13:46:18 -0700

    hopefully it works

---
+++ src/App.js
@@ -0,0 +1,115 @@
+/*For some reason, when I set the tiles to be null or empty at the beginning, when they got filled in, the tiles began to move around
+  So, I changed the code to fill them in with a *, and adjusted code accordingly
+*/
+import { useState } from 'react';
+
+function Square({value, onSquareClick}){
+  return <button className="square" onClick={onSquareClick}>{value}</button>
+ 
+}
+
+function Board({xIsNext, squares, onPlay}){
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext? "X": "O");
+  }
+  function handleClick(i){
+    if(squares[i] != "*"  || calculateWinner(squares)){
+      return;
+    }
+    const nextSquares = squares.slice();
+    if(xIsNext){
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
+  return (
+    <div id="center">
+      <h1>Tic-Tac-Toe</h1>
+      <p>Note that * means that space is available!</p>
+      <div className="status">
+        <h3>{status}</h3>
+      </div>
+      <div className="board-row">
+       <Square value={squares[0]} onSquareClick={() =>handleClick(0)}/>
+       <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
+       <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+       </div>
+       <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick ={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick ={() => handleClick(5)}/>
+       </div>
+       <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick ={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick ={() => handleClick(8)} />
+       </div>
+    </div>
+  )
+}
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const[history, setHistory] = useState([Array(9).fill("*")]); 
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove]
+  
+  function handlePlay(nextSquares){
+      const nextHistory = [...history.slice(0,currentMove + 1), nextSquares]
+      setHistory(nextHistory);
+      setCurrentMove(nextHistory.length -1);
+      setXIsNext(!xIsNext);
+  }
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0){
+      description = "Go to move # " + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <h2>Past Moves</h2>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+function calculateWinner(squares){
+  const lines=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+  for(let i=0; i<lines.length;i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] == squares[b] && squares[a] == squares[c] && squares[a] != "*"){
+      return squares[a];
+    }
+  }
+  return null;
+}
